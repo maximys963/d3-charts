b4bdb2b0595939080f0ae7bcf3784ee8fb71bb87
maxym.panasenko <maxym.panasenko@solidgate.com>
2022-07-17 17:54:53 +0300

    add choropleth chart setup

---
+++ choropleth/src/index.ts
@@ -0,0 +1,7 @@
+import eductionDS from './dataset/educationDS.json'
+import countiesDS from './dataset/countiesDS.json'
+
+import './main.css'
+
+
+
---
+++ choropleth/webpack.common.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html',
+    })
+  ],
+}
